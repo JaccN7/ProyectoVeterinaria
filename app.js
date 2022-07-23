f275a022067b3638a739f7bae051a02953a159ce
Javiera Cortés Celedón <javieracortesceledon@gmail.com>
2022-07-23 03:21:46 -0400

    Proyecto veterinaria inicializado

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const url = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PW}@cluster0.dluii.mongodb.net/${process.env.MONGO_ATLAS_BD}?retryWrites=true&w=majority`
+
+console.log(__dirname + '\\public')
+//Conexión a BD MongoDB
+mongoose.connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conexión a BD MongoDB exitosa');
+}).catch(err => {
+    console.log('Error al conectar a BD MongoDB');
+    console.log(err);
+});
+
+//Motor de plantillas EJS | Rutas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+//Carpeta publica | Ruta
+app.use('/public', express.static(__dirname + '\\public'));
+
+//Rutas 
+app.use('/', require('./router/loginRoutes'));
+app.use('/dashboardPets', require('./router/mascotasRoutes'));
+//manejo de errores
+app.use('/', require('./router/errorRoutes'));
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}, el enlace es http://localhost:${port}`);
+});
