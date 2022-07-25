1126201705adcc7a8a04ea2304baba3c99bfdd87
Javiera Cortés Celedón <javieracortesceledon@gmail.com>
2022-07-24 23:23:53 -0400

    ruta de autentificacion agregada a app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
-
+//morgan
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.json());
 
 const url = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PW}@cluster0.dluii.mongodb.net/${process.env.MONGO_ATLAS_BD}?retryWrites=true&w=majority`
 
@@ -32,8 +32,9 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(path.join(__dirname, './public')))
 
 //Rutas 
-app.use('/', require('./router/loginRoutes'));
+app.use('/', require('./router/loginRoute'));
 app.use('/dashboardPets', require('./router/mascotasRoutes'));
+app.use('/authUsers', require('./router/authUsers'));
 //manejo de errores
 app.use('/', require('./router/errorRoutes'));
 
